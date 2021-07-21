7362dfd538a5edbef95ac2e61b1aee039d345d6e
connorivy <43247197+connorivy@users.noreply.github.com>
2021-07-21 10:45:42 -0500

    fixed untracked files

--- model/models.py
+++ model/models.py
@@ -1,23 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Member(models.Model):
-    id = models.CharField(max_length=20)
+    id = models.CharField(primary_key=True, max_length=20)
     shape = models.CharField(max_length=15)
-    node1 = {
-        models.CharField(max_length=15),
-        models.CharField(max_length=15),
-        models.CharField(max_length=15),
-    }
-    node2 = {
-        models.CharField(max_length=15),
-        models.CharField(max_length=15),
-        models.CharField(max_length=15),
-    }
+
+    node1x = models.CharField(max_length=15),
+    node1y = models.CharField(max_length=15),
+    node1z = models.CharField(max_length=15),
+
+    node2x = models.CharField(max_length=15),
+    node2y = models.CharField(max_length=15),
+    node2z = models.CharField(max_length=15),
 
     BoolType = models.TextChoices('MedalType', 'True False')
 
     designed = models.CharField(blank=True, choices=BoolType.choices, max_length=10)
 
-    last_modified_at = models.DateTimeField()
+    last_modified_at = models.DateTimeField(auto_now=True)
 
-    last_modified_by = models.CharField()
+    last_modified_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+   return self.id, self.shape 
--- model/views.py
+++ model/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Member
 
 # Create your views here.
 def home(request):
+    context = {
+        'members': Member.objects.all()
+    }
     return render(request, 'model/model.html') 
