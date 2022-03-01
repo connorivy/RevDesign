de0e58d54b0e3e88959f3bfb2b1f3b5f70a74ce8
connorivy <connorivy15@utexas.edu>
2022-03-01 17:52:15 -0600

    traction forces working

--- model/requests.py
+++ model/requests.py
@@ -2,6 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http.response import JsonResponse
 from .models import *
 import pygmsh
+import numpy as np
 import json
 
 @csrf_exempt
@@ -9,8 +10,8 @@ def get_floor_mesh(request):
     if request.is_ajax and request.method == "POST":
         # get the floor coord_list from the client side.
         coord_list_floor = json.loads(request.POST.get('coord_list_floor'))
-        coord_list_walls = request.POST.get('coord_list_walls')
-        print(coord_list_floor, coord_list_walls)
+        coord_list_walls = json.loads(request.POST.get('coord_list_walls'))
+        # print(coord_list_floor, coord_list_walls, request.POST)
 
         # This will be a huge job, but eventually I need to combine my Django and speckle servers
         # then I wouldn't be passing the floor coord_list_floor into this function,
@@ -19,9 +20,16 @@ def get_floor_mesh(request):
         # Must run django without multithreading in order to do this (python manage.py runserver --nothreading --noreload). Who knows how we'll handle this in deployment
         polygon = []
         with pygmsh.geo.Geometry() as geom:
+            # add lines from floor
             for coord in coord_list_floor:
                 polygon.append([coord['x'], coord['y']])
-            geom.add_polygon(polygon, mesh_size=1)
+            geom.add_polygon(polygon, mesh_size=.5)
+
+            # add points from shear walls
+            for wall in coord_list_walls:
+                points = get_points_from_wall(wall, .1)
+                for point in points:
+                    geom.add_point(point)
             mesh = geom.generate_mesh()
 
         # with pygmsh.geo.Geometry() as geom:
@@ -35,7 +43,14 @@ def get_floor_mesh(request):
         #         mesh_size=0.1,
         #     )
         #     mesh = geom.generate_mesh()
-        mesh.write('test1.vtk')
+        mesh.write('test1.mesh')
 
         return JsonResponse({'success?': 'yes'}, status = 200)
     return JsonResponse({}, status = 400)
+
+def get_points_from_wall(wall, mesh_size=1):
+    a = np.array([float(wall[0]), float(wall[1])])
+    b = np.array([float(wall[2]), float(wall[3])])
+    parts = np.ceil(np.linalg.norm(a-b) / mesh_size) + 1
+    print(np.linspace(a,b,int(parts)).tolist())
+    return np.linspace(a,b,int(parts)).tolist()
